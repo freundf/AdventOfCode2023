bf36eb9dd2bad09289283d76b49eaad0a905e7b9
Fabio Freund <fabio@freund.io>
2023-12-08 13:31:24 +0100

    improved day 8

--- 8.py
+++ 8.py
@@ -1,7 +1,9 @@
 from typing import Callable
+from math import lcm
+import re
 
 from read_input import read_input
-from math import lcm
+
 
 DAY = 8
 
@@ -13,13 +15,11 @@ def part1() -> int:
     return iterate_maps('AAA', check_end_part1, maps, instructions)
 
 
-def parse_maps(lines: list[str]) -> dict[tuple[str, str]]:
+def parse_maps(lines: list[str]) -> dict[str, tuple[str, str]]:
     maps = {}
+    line_regex = re.compile("(\w{3}) = \((\w{3}), (\w{3})\)")
     for line in lines:
-        name, directions = line.split(" = ")
-        left, right = directions.split(", ")
-        left = left[1:]
-        right = right[:3]
+        name, left, right = list(line_regex.search(line).groups())
         maps[name] = (left, right)
 
     return maps
@@ -29,18 +29,15 @@ def check_end_part1(current: str) -> bool:
     return current == 'ZZZ'
 
 
-def iterate_maps(start: str, end_check: Callable[[str], bool], maps: dict[tuple[str, str]], instructions) -> int:
-    map_next = start
+def iterate_maps(start: str, end_check: Callable[[str], bool], maps: dict[str, tuple[str, str]], instructions) -> int:
+    map_name = start
     count = 0
     while True:
         for direction in instructions:
-            if direction == 'L':
-                map_next = maps[map_next][0]
-            else:
-                map_next = maps[map_next][1]
-
+            map_next = maps[map_name]
+            map_name = map_next[0] if direction == 'L' else map_next[1]
             count += 1
-            if end_check(map_next):
+            if end_check(map_name):
                 return count
 
 
