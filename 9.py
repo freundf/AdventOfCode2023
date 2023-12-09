b448ec26bb6de197618fcff914d1c7b5f706745a
Fabio Freund <fabio@freund.io>
2023-12-09 11:06:23 +0100

    add oneliner for part1 and part2 for fun

--- 9.py
+++ 9.py
@@ -1,5 +1,5 @@
 from read_input import read_input
-
+import sys
 DAY = 9
 
 
@@ -15,13 +15,17 @@ def part2() -> int:
     return sum(calc_next_num(sequence) for sequence in sequences)
 
 
+def part1_2_oneliner() -> None:
+    print(*[(lambda x: sum((f := lambda s: 0 if sum(s) == 0 else s[-1] + f([s[i] - s[i - 1] for i in range(1, len(s))]))(sequence) for sequence in [[int(n) for n in line.split(' ')[::x]] for line in open("input/9")]))(direction) for direction in [1, -1]], sep='\n')
+
+
 def calc_next_num(sequence: list[int]) -> int:
     if sum(sequence) == 0:
         return 0
-    sequence_next = [sequence[i] - sequence[i - 1] for i in range(1,len(sequence))]
+    sequence_next = [sequence[i] - sequence[i - 1] for i in range(1, len(sequence))]
     return sequence[-1] + calc_next_num(sequence_next)
 
 
 if __name__ == "__main__":
-    # print(part1())
+    print(part1())
     print(part2())
